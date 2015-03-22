fb2223b01e78a2c7a88465dd964b5ee761de3bb1
Florian Kuhn <kuhn@ids-mannheim.de>
2015-03-22 12:58:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# openslawsgii
+## connecting openlaws to the german federal law
+
+there is a hands-on in the notebook/ folder.
+
+The giigraph module is based on a prototype written during a code camp of
+the openslaws.eu project at the university of applied sciences Salzburg,
+Austria (March 20st and 21st 2015).
+
+The open data project 'openlaws.eu' is aiming at providing legal
+knowledge freely and sophistically processed by using state of the art
+technologies. Since the project is based on an european level, it also
+tries to interlink as many european legal domains as possible.
+
+The german legal domain has not yet been linked to openlaws and my task
+for the hackaton was to prototype the converter that takes the data provided
+on www.gesetze-im-internet.de and converts it to the geoff format of
+neo4j by using the specific attribute model for a database node.
+
+Since time was short, I sticked to just converting the copyright-law file
+as a baseline.
+
+GiiGraph is a class derived from MultiDiGraph of networkx so it can be
+easily enhanced lateron to meet requirements like backreference, crossreference,
+in-place reference, versioning etc.
+
+First of all, I parsed the xml file with the well known lxml module
+(a binding to the libxml2 c-library). The important data items were read using the
+builtin xpath capabilities.
+
+The graph was easily built after looking up the important german data
+items that have to be put in its place in the openlaws attribute model.
+
+The root of all elements is the catalogue metadata of the law itself.
+
+By using conditionals for the metafile structure, only articles were
+put into the graph. They all are chained via edges. Moreover, each article
+node is linked to the root element.
+
+The package neonx is a converter to the json string of neo4j's geoff format.
+It was used to export the networkx graph to the geoff format.
+
+As you can see, the class definition is quite small and readable by
+using already existing open source packages. Besides, the attribute model
+of openlaws is restricted to the very core of important information.
+
+
+### However
+The gii website does not have an API to optain the data without any problems.
+A webcrawler must be written first to get all items in place. I will be
+using scrapy. 
+
+
+
+
---
+++ src/openlawsgii/giigraph.py
@@ -0,0 +1,89 @@
+__author__ = 'kuhn'
+__doc__ = 'a hack to convert gii xml data to openlaws-neo4j'
+import codecs
+import networkx
+import neonx
+from lxml import etree
+
+# TODO: find a non hardoced solution
+LAWMETAXPATH = "/dokumente/norm/metadaten"
+
+class GiiGraph(networkx.MultiDiGraph):
+    """
+    GiiGraph represents the attribute Node structure
+    """
+
+    def __init__(self):
+        super(GiiGraph, self).__init__()
+        """
+
+        :return:
+        """
+
+    def add_gii_xmlfile(self, xmlfile):
+        """
+        reads an gii xml file to an networkx MultiDiGraph
+        :param xmlfile:
+        :return:
+        """
+        giixmlfileobject = codecs.open(xmlfile, encoding="utf-8")
+        giietree = etree.parse(giixmlfileobject)
+
+        # define the root node of the graph
+        self.add_german_law_object(giietree)
+        self.add_german_article(giietree)
+
+    def add_german_law_object(self, giietree):
+        """
+        adds the etree of an law object to the graph structure
+        :param giietree:
+        :return:
+        """
+        metainfo = giietree.xpath(LAWMETAXPATH)[0]
+
+        self.add_node("GermanLegalObject", {"name_of_legal_object": metainfo.xpath("./kurzue")[0].text,
+                                            "author": metainfo.xpath("./fundstelle/periodikum")[0].text,
+                                            "description": metainfo.xpath("./langue")[0].text,
+                                            "type": "Bundesgesetz",
+                                            "date_of_document": giietree.xpath("/dokumente/norm[1]/@builddate")[0],
+                                            "date_of_effect": metainfo.xpath("./ausfertigung-datum")[0].text,
+                                            "external_ID": giietree.xpath("/dokumente/norm[1]/@doknr")[0],
+                                            "link_to_document": "URI"})
+
+    def add_german_article(self, giietree):
+        """
+        adds the etree to the graph structure
+        :param giietree:
+        :return:
+        """
+        # /dokumente/norm[37] /dokumente/norm[10]
+        # find the articles level in the etree object
+        count = 0
+        for normelement in giietree.xpath("/dokumente/norm"):
+
+            if len(normelement.xpath("./metadaten/enbez")) > 0 and len(normelement.xpath("./metadaten/titel")) > 0:
+                # <!ELEMENT metadaten (jurabk+, amtabk?, ausfertigung-datum?, fundstelle*, kurzue?, langue?, gliederungseinheit?, enbez?, titel?, standangabe*)>
+
+                self.add_node("GermanLegalArticle_" + str(count),
+                                                                 {
+                                                                    "external_ID": normelement.xpath("./metadaten/enbez")[0].text,
+                                                                    "name_of_legal_object": normelement.xpath("./metadaten/titel")[0].text,
+                                                                    "author": giietree.xpath("/dokumente/norm/metadaten/fundstelle/periodikum")[0].text,
+                                                                    "date_of_effect": giietree.xpath("/dokumente/norm[1]/metadaten/ausfertigung-datum")[0].text,
+                                                                    "date_of_document": normelement.xpath("./@builddate")[0],
+                                                                    "link_to_document": "URI",
+                                                                    "entire_text_of_legal_article": [p.text for p in normelement.xpath("./textdaten/text/Content/P")]}
+                                                                 )
+
+                self.add_edge("GermanLegalObject", "GermanLegalArticle_" + str(count))
+                count += 1
+            if count > 0:
+                self.add_edge("GermanLegalArticle_" + str(count-1), "GermanLegalArticle_" + str(count))
+
+
+    def export2geoff(self):
+        """
+        export the networkx representation to neo4j geoff format
+        """
+        gdata = neonx.get_geoff(self, "LINKS_TO")
+        return gdata
